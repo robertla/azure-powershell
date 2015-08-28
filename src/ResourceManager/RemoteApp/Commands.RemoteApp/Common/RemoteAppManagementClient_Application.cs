2337186401732612e439209124d44321f72abb25
Microsoft <robertla@microsoft.com>
2015-08-28 06:58:38 -0700

    fix merge conflicts

--- src/ResourceManager/RemoteApp/Commands.RemoteApp/Common/RemoteAppManagementClient_Application.cs
+++ src/ResourceManager/RemoteApp/Commands.RemoteApp/Common/RemoteAppManagementClient_Application.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Azure.Management.RemoteApp;
+﻿using Microsoft.Azure.Commands.RemoteApp.Cmdlet;
+using Microsoft.Azure.Management.RemoteApp;
 using Microsoft.Azure.Management.RemoteApp.Models;
 using System.Collections.Generic;
+using System.Management.Automation;
 
 namespace Microsoft.Azure.Commands.RemoteApp.Common
 {
@@ -9,7 +11,6 @@ namespace Microsoft.Azure.Commands.RemoteApp.Common
         internal PublishedApplicationDetails GetApplication(string resourceGroupName, string collectionName, string alias)
         {
             return Client.Collection.GetPublishedApp(collectionName, alias, resourceGroupName);
-
         }
 
         internal IList<PublishedApplicationDetails> GetApplications(string resourceGroupName, string collectionName)
@@ -25,19 +26,34 @@ namespace Microsoft.Azure.Commands.RemoteApp.Common
         internal StartMenuApplication GetStartMenuApp(string resourceGroupName, string collectionName, string applicationId)
         {
             return Client.Collection.GetStartMenuApp(applicationId, collectionName, resourceGroupName);
-
         }
 
         internal PublishingOperationResult PublishApp(string resourceGroupName, string collectionName, ApplicationDetails details)
         {
-            return Client.Collection.PublishOrUpdateApplication(details, collectionName, details.Alias, resourceGroupName);
+            PublishedApplicationDetails publishedApp = null; 
+            
+            publishedApp = Client.Collection.GetPublishedApp(collectionName, details.Alias, resourceGroupName);
+            if (publishedApp != null)
+            {
+                RemoteAppServiceException ex = new RemoteAppServiceException(
+                                                Commands_RemoteApp.ApplicationExists,
+                                                ErrorCategory.InvalidArgument
+                                            );
+                throw ex;
+            }
 
+            return Client.Collection.PublishOrUpdateApplication(details, collectionName, details.Alias, resourceGroupName);
+        }
+
+
+        internal PublishingOperationResult ModifyApp(string resourceGroupName, string collectionName, ApplicationDetails details)
+        {
+            return Client.Collection.PublishOrUpdateApplication(details, collectionName, details.Alias, resourceGroupName);
         }
 
         internal PublishingOperationResult UnpublishApp(string resourceGroupName, string collectionName, string alias)
         {
             return Client.Collection.Unpublish(collectionName, alias, resourceGroupName);
-
         }
     }
 }
